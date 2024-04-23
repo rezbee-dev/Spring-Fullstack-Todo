c09bfa13495508aae05170e709ff7a562e34212d
devrisby <118779687+devrisby@users.noreply.github.com>
2024-04-23 08:50:53 -0400

    modify default error response

---
+++ backend/src/main/java/com/risby/todoapp/http/FallbackErrorAttributes.java
@@ -0,0 +1,20 @@
+package com.risby.todoapp.http;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+// src: https://www.baeldung.com/exception-handling-for-rest-with-spring#spring-boot
+@Component
+public class FallbackErrorAttributes extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String,Object> errorAttributes =  super.getErrorAttributes(webRequest, options);
+        errorAttributes.remove("message");
+        errorAttributes.remove("path");
+        return errorAttributes;
+    }
+}
