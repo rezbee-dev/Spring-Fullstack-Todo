9f5d09416f726e5c77942ca0d8892aa5b2bd6ce9
devrisby <118779687+devrisby@users.noreply.github.com>
2024-05-08 04:17:55 -0400

    init vite configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // allows listening on all addresses (for backend API during dev ?)
+    host: true,
+    port: 8081,
+    // exits app if port already in use, instead of trying next available port
+    strictPort: true, 
+    // overcomes cors issues
+    // allows for using relative URL in API calls
+    // see https://github.com/http-party/node-http-proxy#options
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
