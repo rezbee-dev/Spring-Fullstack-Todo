2705a97c05d1604eb732011490919b7460d8d93a
devrisby <118779687+devrisby@users.noreply.github.com>
2024-06-04 17:08:28 -0400

    Refactor & clean up some code

--- backend/src/main/java/com/risby/todoapp/http/SecurityConfig.java
+++ backend/src/main/java/com/risby/todoapp/http/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
