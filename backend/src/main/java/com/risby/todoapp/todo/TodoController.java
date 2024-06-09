c1be35a93721d186782d10e71ae6f30a6cffc143
devrisby <118779687+devrisby@users.noreply.github.com>
2024-06-09 18:16:59 -0400

    add dummy todos & todo paginated endpoint

--- backend/src/main/java/com/risby/todoapp/todo/TodoController.java
+++ backend/src/main/java/com/risby/todoapp/todo/TodoController.java
@@ -1,7 +1,11 @@
 package com.risby.todoapp.todo;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +24,30 @@ public class TodoController {
     }
 
     @GetMapping
-    @Operation(summary = "Returns all todos")
-    public ResponseEntity<List<Todo>> findAll(){
-        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    @Operation(summary = "Returns unsorted Todo items of particular page (default = 0) and size (default = 3)")
+    @Parameter(name = "page", allowEmptyValue = true)
+    @Parameter(name = "size", allowEmptyValue = true)
+    public ResponseEntity<List<Todo>> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "3") int size){
+        PageRequest p = PageRequest.of(page,size);
+        Page<Todo> pg = repo.findAll(p);
+
+        System.out.println(pg.getTotalElements());
+        System.out.println(pg.getTotalPages());
+
+        return new ResponseEntity<>(pg.stream().toList(), HttpStatus.OK);
     }
+
+//    @GetMapping
+//    @Operation(summary = "Returns all todos")
+//    public ResponseEntity<List<Todo>> findAllByCategory(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "3") int size){
+//        PageRequest p = PageRequest.of(page,size);
+//        Page<Todo> pg = repo.findAll(p);
+//
+//        System.out.println(pg.getTotalElements());
+//        System.out.println(pg.getTotalPages());
+//
+//        return new ResponseEntity<>(pg.stream().toList(), HttpStatus.OK);
+//    }
 
     @PostMapping
     @Operation(summary = "Creates new todo")
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,3 +3,17 @@ INSERT INTO country (name) VALUES ('France');
 INSERT INTO country (name) VALUES ('Brazil');
 INSERT INTO country (name) VALUES ('Italy');
 INSERT INTO country (name) VALUES ('Canada');
+-- Todo Items
+INSERT INTO todo (id, title, priority) VALUES (1,'Todo Title 1', 1);
+INSERT INTO todo (id, title, priority) VALUES (2,'Todo Title 2', 2);
+INSERT INTO todo (id, title, priority) VALUES (3,'Todo Title 3', 3);
+INSERT INTO todo (id, title, priority) VALUES (4,'Todo Title 4', 1);
+INSERT INTO todo (id, title, priority) VALUES (5,'Todo Title 5', 2);
+INSERT INTO todo (id, title, priority) VALUES (6,'Todo Title 6', 3);
+INSERT INTO todo (id, title, priority) VALUES (7,'Todo Title 7', 1);
+INSERT INTO todo (id, title, priority) VALUES (8,'Todo Title 8', 2);
+INSERT INTO todo (id, title, priority) VALUES (9,'Todo Title 9', 3);
+INSERT INTO todo (id, title, priority) VALUES (10,'Todo Title 10', 1);
+INSERT INTO todo (id, title, priority) VALUES (11,'Todo Title 11', 2);
+INSERT INTO todo (id, title, priority) VALUES (12,'Todo Title 12', 3);
+INSERT INTO todo (id, title, priority) VALUES (13,'Todo Title 13', 1);
